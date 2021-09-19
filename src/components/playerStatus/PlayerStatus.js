6cb2c15d6e35ff2244b76edea88de722fca7c659
adramelech <adramelech.psd@gmail.com>
2021-09-19 16:30:52 +0530

    none

--- src/components/playerStatus/PlayerStatus.js
+++ src/components/playerStatus/PlayerStatus.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { setCurrentSong, setPlaying } from "../../features/userSlice";
+import { setCurrentSong } from "../../features/userSlice";
 import "./playerStatus.css";
 
 let interval;
